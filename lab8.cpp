b98cacda0082d30b811e3eb944658df053f37cc2
TonySavich <92541859+TonySavich@users.noreply.github.com>
2023-02-14 20:50:31 +0200

    Add files via upload

---
+++ lab8.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+int const N = 6;
+
+struct deistr {
+    int i;
+    int path = 1000000;
+  //  int neibor;
+
+};
+
+int main()
+{
+    int mas[N][N];
+
+
+    std::ifstream in("in.txt");
+    if (in.is_open())
+    {
+        while (!in.eof()) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+
+                    in >> mas[i][j];
+                }
+            }
+
+
+        }
+
+
+    }
+    in.close();
+
+
+    int t, queu[N];
+    
+    t = -1;
+
+    int v[N];
+    deistr dop[N];
+    for (int i = 0; i < N; i++) {
+        v[i] = 0;
+        dop[i].i = i;
+    }
+
+
+    int k;
+    std::cin >> k;
+    std::cout << std::endl;
+    k = k - 1;
+    dop[k].path = 0;
+    
+    int och[N];
+    int rr = -1;
+    int y=0;
+    rr++;
+    och[rr] = k;
+   
+
+
+    while(y != N){
+
+   
+
+    for (int i = 0; i < N; i++) {
+        if (i != k) {
+            if (mas[k][i] != 0) {
+                if (v[i] == 0) {
+                    t++;
+                    queu[t] = i;
+                    bool f = false;
+                    for (int j = 0; j <= rr; j++) {
+                        if (och[j] == i) {
+                            f = true;
+                        }
+                    }
+                    if (f == false) {
+                        rr++;
+                        och[rr] = i;
+                    }
+                }
+
+            }
+        }
+    }
+   
+
+    for (int i = 0; i < t+1; i++) {
+        if (dop[k].path + mas[dop[k].i][queu[i]] < dop[queu[i]].path) {
+            dop[queu[i]].path = dop[k].path + mas[dop[k].i][queu[i]];
+      //      dop[queu[i]].neibor = dop[k].i;
+        }
+    }
+
+
+    v[dop[k].i] = 1;
+    y++;
+    k = och[y];
+
+    for (int i = 0; i <= t; i++) {
+        queu[i] = 0;
+    }
+    t = -1;
+ 
+
+}
+
+    for (int i = 0; i < N; i++) {
+        std::cout << dop[i].i +1<< " --- " << dop[i].path << std::endl;
+        
+   }
+
+}
