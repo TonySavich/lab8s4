0e21d2bedbaa04017455ac2adc0a12fcba6db7b4
TonySavich <92541859+TonySavich@users.noreply.github.com>
2023-02-21 14:57:33 +0200

    Update lab8.cpp

--- lab8.cpp
+++ lab8.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 
 
@@ -7,7 +7,7 @@ int const N = 6;
 struct deistr {
     int i;
     int path = 1000000;
-  //  int neibor;
+    //  int neibor;
 
 };
 
@@ -16,7 +16,7 @@ int main()
     int mas[N][N];
 
 
-    std::ifstream in("in.txt");
+    std::ifstream in("in8.txt");
     if (in.is_open())
     {
         while (!in.eof()) {
@@ -36,7 +36,7 @@ int main()
 
 
     int t, queu[N];
-    
+
     t = -1;
 
     int v[N];
@@ -52,65 +52,76 @@ int main()
     std::cout << std::endl;
     k = k - 1;
     dop[k].path = 0;
-    
+
     int och[N];
     int rr = -1;
-    int y=0;
+    int y = 0;
     rr++;
     och[rr] = k;
-   
 
 
-    while(y != N){
 
-   
+    while (y != N) {
 
-    for (int i = 0; i < N; i++) {
-        if (i != k) {
-            if (mas[k][i] != 0) {
-                if (v[i] == 0) {
-                    t++;
-                    queu[t] = i;
-                    bool f = false;
-                    for (int j = 0; j <= rr; j++) {
-                        if (och[j] == i) {
-                            f = true;
+
+
+        for (int i = 0; i < N; i++) {
+            if (i != k) {
+                if (mas[k][i] != 0) {
+                    if (v[i] == 0) {
+                        t++;
+                        queu[t] = i;
+                        bool f = false;
+                        for (int j = 0; j <= rr; j++) {
+                            if (och[j] == i) {
+                                f = true;
+                            }
+                        }
+                        if (f == false) {
+                            rr++;
+                            och[rr] = i;
                         }
                     }
-                    if (f == false) {
-                        rr++;
-                        och[rr] = i;
-                    }
-                }
 
+                }
             }
         }
-    }
-   
 
-    for (int i = 0; i < t+1; i++) {
-        if (dop[k].path + mas[dop[k].i][queu[i]] < dop[queu[i]].path) {
-            dop[queu[i]].path = dop[k].path + mas[dop[k].i][queu[i]];
-      //      dop[queu[i]].neibor = dop[k].i;
+
+        for (int i = 0; i < t + 1; i++) {
+            if (dop[k].path + mas[dop[k].i][queu[i]] < dop[queu[i]].path) {
+                dop[queu[i]].path = dop[k].path + mas[dop[k].i][queu[i]];
+                //      dop[queu[i]].neibor = dop[k].i;
+            }
         }
+
+
+        v[dop[k].i] = 1;
+        y++;
+
+       for (int i = y; i <= rr; i++) {
+            for (int j = i + 1; j <= rr; j++) {
+                if (dop[och[i]].path > dop[och[j]].path) {
+                    std::swap(och[i], och[j]);
+                }
+            
+            }
+
+        }
+
+        k = och[y];
+
+        for (int i = 0; i <= t; i++) {
+            queu[i] = 0;
+        }
+        t = -1;
+
+
     }
-
-
-    v[dop[k].i] = 1;
-    y++;
-    k = och[y];
-
-    for (int i = 0; i <= t; i++) {
-        queu[i] = 0;
-    }
-    t = -1;
- 
-
-}
 
     for (int i = 0; i < N; i++) {
-        std::cout << dop[i].i +1<< " --- " << dop[i].path << std::endl;
-        
-   }
+        std::cout << dop[i].i + 1 << " --- " << dop[i].path << std::endl;
+
+    }
 
 }
